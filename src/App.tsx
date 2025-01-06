b26a73233f7ec67161da66ea5811382a8dd5f3ff
YBSeok <soberyl4304@gmail.com>
2025-01-06 19:09:04 +0900

    💄Update: add top&footer nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Test from './pages/Test.tsx'
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        
+        <Route path="/" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import "./styles/color.css";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />

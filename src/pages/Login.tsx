3d7a7ecf7c01a1271ed93b8e060fc9d157cf0469
YBSeok <112086285+YBSeok@users.noreply.github.com>
2025-01-11 18:36:10 +0900

    Revert "✨Feature/#8 quest page design"

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,104 +1,42 @@
-
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-import LargeBtn from '../components/button/LargeBtn.tsx';
-import LargeInput from '../components/inputField/LargeInput.tsx';
+import TopNav from '../components/nav/TopNav.tsx';
+import FooterNav from '../components/nav/FooterNav.tsx'
 
-import ID from '../assets/images/gray_person.png'
-import ActID from '../assets/images/lightgray_person.png'
-import Lock from '../assets/images/gray_lock.png'
-import ActLock from '../assets/images/lightgray_lock.png'
 
-function Login() {
-    const navigate = useNavigate();
+function ChatList() {
 
-    const [userID, setUserID] = useState('');
-    const [userPWD, setUserPWD] = useState('');
-    const [isLoginAvailable, setIsLoginAvailable] = useState(false);
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, setInput: React.Dispatch<React.SetStateAction<string>>) => {
-        setInput(event.target.value);
+    const center = {
+        icon: null,
+        text: "username",
+        clickFunc: null
     };
 
-    const handleLogin = () => {
-        // 로그인 성공 시 /home으로 네비게이트
-        if (isLoginAvailable) {
-            // 여기서 로그인 로직을 추가할 수 있습니다.
-            // 예를 들어, API 호출 후 성공 여부에 따라 네비게이트
-            navigate('/home');
-        }
-    };
-
-    useEffect(() => {
-        setIsLoginAvailable(userID !== '' && userPWD !== '');
-    }, [userID, userPWD]);
 
     return (
-
-        <LoginScreenContainer>
-            <Header className='title-lg-300'>
-                로그인
-            </Header>
-
-            <InputContainer>
-                <LargeInput
-                    icon={ID}
-                    activeIcon={ActID}
-                    placeholder="아이디를 입력해주세요."
-                    value={userID}
-                    onChangeFunc={(e) => handleInputChange(e, setUserID)}
-                />
-
-                <LargeInput
-                    icon={Lock}
-                    activeIcon={ActLock}
-                    placeholder="비밀번호를 입력해주세요."
-                    type="password"
-                    value={userPWD}
-                    onChangeFunc={(e) => handleInputChange(e, setUserPWD)}
-                />
-            </InputContainer>
-
-            <LargeBtn
-                content="로그인"
-                onClick={handleLogin}
-                isAvailable={isLoginAvailable}
+        <div>
+            <TopNav
+                lefter={null}
+                center={center}
+                righter={null}
             />
 
-        </LoginScreenContainer>
-
+            <FooterNav></FooterNav>
+        </div>
     );
 }
 
-export default Login;
+export default ChatList;
 
-const LoginScreenContainer = styled.div`
+const ChatPersonList = styled.div`
 display: flex;
 flex-direction: column;
-align-items: center;
-
-padding: 0 15px;
-overflow: hidden;
-`;
-
-const Header = styled.div`
-
-margin: 111px 0;
-
-color: var(--gray-100);
-`;
-
-const InputContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
 justify-content: center;
+align-items: center;
 
-width: 100%;
+gap: 10px;
+padding: 20px;
 
-gap: 15px;
-
-margin-bottom: 40px;
 `;
